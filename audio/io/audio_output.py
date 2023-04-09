3d6be33bfc73e855ae610aa0eea2be637ea6fd92
Julien Wallart <julien.wallart@outlook.com>
2023-04-09 18:25:21 +0200

    Force interrupt when playing sounds

--- audio/io/audio_output.py
+++ audio/io/audio_output.py
@@ -7,6 +7,7 @@ from librosa import resample
 
 import queue
 import logging
+import sounddevice as sd
 
 
 class AudioOutput(SoundDeviceResource, Consumer):
@@ -21,6 +22,11 @@ class AudioOutput(SoundDeviceResource, Consumer):
         super().stop()
         self.close()
 
+    def quiet(self):
+        with self._in_queue.mutex:
+            self._in_queue.queue.clear()
+        sd.stop()
+
     def run(self) -> None:
         self.open()
         while self._stream.active:
@@ -30,8 +36,11 @@ class AudioOutput(SoundDeviceResource, Consumer):
 
                 # audio = int16_to_float32(audio)
                 audio = resample(int16_to_float32(audio), orig_sr=self.sample_rate, target_sr=self.device_default_sr)
-                self._stream.write(audio)
                 # self._previously_played = audio
+                try:
+                    self._stream.write(audio)
+                except Exception:
+                    ProjectLogger().error('Output audio stream not available.')
 
                 logging.info(f'{self.__class__.__name__} {time() - t0:.3f} exec. time')
             except queue.Empty:
