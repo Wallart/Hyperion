2cf0ce06393356896d4e7aa75616ffc36dff2091
Julien Wallart <julien.wallart@outlook.com>
2023-04-09 18:25:09 +0200

    Fixed poping sounds

--- audio/speakers_stream.py
+++ audio/speakers_stream.py
@@ -23,7 +23,6 @@ class SpeakersStream(Consumer):
             audio = self._in_queue.get()
             t0 = time()
 
-            audio = audio * ((2 ** 16) / 2) / max(audio)
-            self._speakers.write(audio.astype(np.int16))
+            self._speakers.write(audio[100:])
             # sd.play(audio, blocking=False, samplerate=self.sample_rate)
             logging.info(f'{self.__class__.__name__} {time() - t0:.3f} exec. time')
