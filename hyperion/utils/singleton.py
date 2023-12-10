7416ed7c0a659df0d702410a47838d614bec9a5e
Julien Wallart <julien.wallart@outlook.com>
2023-12-10 03:27:48 +0100

    Fixed singleton issue

--- hyperion/utils/singleton.py
+++ hyperion/utils/singleton.py
@@ -2,6 +2,7 @@ class Singleton(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+        # better use class name as key, because cls can vary depending of package import
+        if cls.__name__ not in cls._instances:
+            cls._instances[cls.__name__] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls.__name__]
